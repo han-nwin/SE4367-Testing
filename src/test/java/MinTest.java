6212930467a4241689e3c53198f1d83ab9094039
han-nwin <hannguyen.win@gmail.com>
2026-02-23 10:05:06 -0600

    mintest

--- src/test/java/MinTest.java
+++ src/test/java/MinTest.java
@@ -22,6 +22,49 @@ public class MinTest {
         }
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testForNullElement() {
+        list.add(null);
+        list.add("cat");
+        Min.min(list);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testForSoloNullElement() {
+        list.add(null);
+        Min.min(list);
+    }
+
+    @Test(expected = ClassCastException.class)
+    @SuppressWarnings("unchecked")
+    public void testMutuallyIncomparable() {
+        List list = new ArrayList();
+        list.add("cat");
+        list.add("dog");
+        list.add(1);
+        Min.min(list);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyList() {
+        Min.min(list);
+    }
+
+    @Test
+    public void testSingleElement() {
+        list.add("cat");
+        Object obj = Min.min(list);
+        assertTrue("Single Element List", obj.equals("cat"));
+    }
+
+    @Test
+    public void testDoubleElement() {
+        list.add("dog");
+        list.add("cat");
+        Object obj = Min.min(list);
+        assertTrue("Double Element List", obj.equals("cat"));
+    }
+
     @After
     public void tearDown() {
         list = null;
